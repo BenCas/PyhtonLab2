8d0de8b1db5100ef6f1e6151e322d7967987ed35
BenCas <38674334+BenCas@users.noreply.github.com>
2019-03-10 09:31:26 -0700

    Add files via upload

---
+++ Lab2.py
@@ -0,0 +1,16 @@
+purchase1 = float(input(“price 1= $’))
+print(purchase1)
+purchase2 = float(input(“price 2= $’))
+print(purchase2)
+purchase3 = float(input(“price 3 = $’))
+print(purchase3)
+
+subTotal = (purchase1 + purchase2 + purchase3)
+print “subtotal = $” +str(subtotal)
+
+salesTax = subtotal * 0.06
+print “sales tax = $” +str(salesTax)
+
+totalAmount = (Subtotal + SalesTax)
+
+print “Total = $” +str(totalAmount)
